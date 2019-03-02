a6fa171c528306fa5c5444b0f3b7938af9c86b7b
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-03-02 09:23:05 -0300

    adiciona fluxo de para envio de email de boas vindas

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/welcome_to_your_cont', methods=['GET', 'POST'])
+def welcome_to_your_cont():
+    if request.method == 'POST':
+        return redirect('/')
+    else:
+        return render_template('welcome_to_your_cont.html')
 
 
 if __name__ == "__main__":
